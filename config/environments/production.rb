8f835a68a81cb9b6ad84e787b866805e71c6f716
jeinieldjs <silvestrejeiniel@gmail.com>
2024-02-23 14:08:27 +0800

    Modified ENV variables.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
   port:            587,
   domain:          'google.com',
   user_name:       ENV.fetch('GMAILACCT'),
-  password:        ENV.fetch('GMAIPWORD'),
+  password:        ENV.fetch('GMAIPASSWORD'),
   authentication:  'plain',
   enable_starttls: true,
   open_timeout:    5,
