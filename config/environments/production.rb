1e6388087f8b56a33f651822fad1f3a4a61f285e
jeinieldjs <silvestrejeiniel@gmail.com>
2024-02-23 13:53:13 +0800

    Added mailer function in production env.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,15 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:         'smtp.gmail.com',
+  port:            587,
+  domain:          'google.com',
+  user_name:       ENV.fetch('GMAILACCT'),
+  password:        ENV.fetch('GMAIPWORD'),
+  authentication:  'plain',
+  enable_starttls: true,
+  open_timeout:    5,
+  read_timeout:    5 }
 end
