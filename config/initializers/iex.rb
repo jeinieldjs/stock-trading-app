dc36b53728f1681f253b83326b03cf1a5231f43c
jeinieldjs <silvestrejeiniel@gmail.com>
2024-02-27 15:10:17 +0800

    Modified iex creds.

--- config/initializers/iex.rb
+++ config/initializers/iex.rb
@@ -1,6 +1,6 @@
 IEX::Api.configure do |config|
-    config.publishable_token = 'pk_4618814643b7465c9f0093862a183c94'
-    config.secret_token = 'sk_fc8caac33b8a4628906ac81ba8f8c62d'
+    config.publishable_token = 'pk_d6c27bfbdeb841e1bcf06bbad998b096'
+    config.secret_token = 'sk_7f6ff8ec74404f839611d4edddfa1a67'
     config.endpoint = 'https://cloud.iexapis.com/v1' 
   end
   
